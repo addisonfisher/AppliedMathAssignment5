30e7b4838cbe262b3499ef2ad1f8f124455f2304
Josephvazhae1 <87318178+Josephvazhae1@users.noreply.github.com>
2025-11-17 18:28:40 -0500

    idk

--- modal_analysis.m
+++ modal_analysis.m
@@ -13,7 +13,7 @@ function modal_analysis(Veq, A, my_rate_func)
         epsilon = 0.1;
 
         V0 = Veq + epsilon*[U_mode;0;0;0];
-        tspan = [0 20];
+        tspan = [0 2];
 
         %run the integration of nonlinear system
         [tlist_nonlinear,Vlist_nonlinear] = ode45(my_rate_func,tspan,V0);
@@ -34,29 +34,31 @@ function modal_analysis(Veq, A, my_rate_func)
 
         figure();
         
-        plot(tlist_nonlinear, Vlist_nonlinear(1,:), 'b');
-        hold on;
-        plot(tlist_lin, Vlist_lin(1,:), 'r--');
-        plot(tlist_nonlinear, x_modal, 'k:');
-        ylabel('X(t)');
-        xlabel('Time (s)');
-        legend('Nonlinear','Linear','Modal');
 
-        plot(tlist_nonlinear, Vlist_nonlinear(2,:), 'b');
+        plot(tlist_nonlinear, x_modal, 'k:');
         hold on;
-        plot(tlist_lin, Vlist_lin(2,:), 'r--');
-        plot(tlist_nonlinear, y_modal, 'k:');
-        ylabel('Y(t)');
-        xlabel('Time (s)');
-        legend('Nonlinear','Linear','Modal');
-        
-        plot(tlist_nonlinear, Vlist_nonlinear(3,:), 'b');
-        plot(tlist_lin, Vlist_lin(3,:), 'r--');
+        plot(tlist_nonlinear, y_modal, 'k:');    
         plot(tlist_nonlinear, theta_modal, 'k:');
 
-        ylabel('\theta(t)');
+        ylabel('Distance');
         xlabel('Time (s)');
-        legend('Nonlinear','Linear','Modal');
+        legend('X','Y','\theta');
+        title('Modal Analysis')
+
+        figure()
+        plot(tlist_nonlinear, Vlist_nonlinear(1,:));
+        hold on;
+        plot(tlist_lin, Vlist_lin(1,:));
+        plot(tlist_nonlinear, Vlist_nonlinear(2,:));
+        plot(tlist_lin, Vlist_lin(2,:));
+        plot(tlist_nonlinear, Vlist_nonlinear(3,:));
+        plot(tlist_lin, Vlist_lin(3,:));
+        title('Linear vs Nonlinear Approximation')
+        ylabel('Distance from Equilibrium');
+        xlabel('Time (s)');
+        legend('nonlinX', 'linX', 'nonlinY', 'linY', 'nonlin\theta', 'lin\theta');
+
         hold off;
     end
+
 end
